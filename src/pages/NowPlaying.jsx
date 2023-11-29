79e723db51f934b077f1f480a8e17bb8840148bd
김용민 <dydals3440@gmail.com>
2023-11-29 22:59:53 +0900

    build수정

--- src/pages/NowPlaying.jsx
+++ src/pages/NowPlaying.jsx
@@ -1,19 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Movie from '../components/Movie';
 import { AppContainer } from '../style/Page.style';
 import Loading from '../components/Loading';
 
 export default function NowPlaying() {
-  const [movies, setMovies] = useState([]); //movie 데이터 저장
+  const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [smallLoading, setSmallLoading] = useState(false); //loading을 결정
-  const [page, setPage] = useState(1); // 보여주려고 하는 페이지
-  const [isFetching, setFetching] = useState(false); //fetching을 할지 안할지 결정
-  const [hasNextPage, setNextPage] = useState(true); // fetching할 페이지가 있는지 없는지 결정
+  const [smallLoading, setSmallLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [isFetching, setFetching] = useState(false);
+  const [hasNextPage, setNextPage] = useState(true);
 
   const fetchData = async (page) => {
     setFetching(true);
-    //데이터 불러오기 전까지 loading
+
     try {
       const result = await fetch(
         `https://api.themoviedb.org/3/movie/now_playing?language=ko&page=${page}`,
@@ -32,39 +32,35 @@ export default function NowPlaying() {
 
       const data = await result.json();
 
-      setMovies((prevMovies) => [...prevMovies, ...data.results]); //이전의 데이터에 지금 가져온 데이터까지 붙임.
-      setNextPage(data.page < 500); //다음 페이지가 있는가? > true //lodaing  끝 > 데이터 출력
-      setFetching(false);
+      setMovies((prevMovies) => [...prevMovies, ...data.results]);
+      setNextPage(data.page < 500);
     } catch (error) {
       console.error('Error fetching data:', error);
+    } finally {
+      setFetching(false);
     }
   };
 
-  //처음 페이지 보여줄 때
-  useEffect(() => {
-    setLoading(true);
-    fetchData(page).then(() => setLoading(false));
-  }, [page]);
-
-  // 만약 화면 아래에 스크롤이 닿는다면
-  const handleScroll = () => {
+  const handleScroll = useCallback(() => {
     const scrollHeight = document.documentElement.scrollHeight - 50;
     const scrollTop = document.documentElement.scrollTop;
     const clientHeight = document.documentElement.clientHeight;
 
     if (scrollTop + clientHeight >= scrollHeight && !isFetching) {
-      setFetching(true);
-      setSmallLoading(true);
-      setPage((prevPage) => prevPage + 1); //page를 변경
+      setPage((prevPage) => prevPage + 1);
     }
-  };
-  // page가 변경되면
+  }, [isFetching]);
+
+  useEffect(() => {
+    setLoading(true);
+    fetchData(page).then(() => setLoading(false));
+  }, [page]);
+
   useEffect(() => {
     if (isFetching && hasNextPage) {
+      setSmallLoading(true);
       fetchData(page).then(() => setSmallLoading(false));
-    } else if (!hasNextPage) {
     }
-    setFetching(false);
   }, [page, hasNextPage, isFetching]);
 
   useEffect(() => {
@@ -77,22 +73,19 @@ export default function NowPlaying() {
       {loading ? (
         <Loading />
       ) : (
-        <AppContainer>
-          {smallLoading ? (
-            <smallLoading />
-          ) : (
-            movies.map((item) => (
-              <Movie
-                key={item.id}
-                title={item.title}
-                poster_path={item.poster_path}
-                vote_average={item.vote_average}
-                overview={item.overview}
-                movieId={item.id}
-              />
-            ))
-          )}
-        </AppContainer>
+        <>
+          {movies.map((item) => (
+            <Movie
+              key={item.id}
+              title={item.title}
+              poster_path={item.poster_path}
+              vote_average={item.vote_average}
+              overview={item.overview}
+              movieId={item.id}
+            />
+          ))}
+          {smallLoading && <Loading />}
+        </>
       )}
     </AppContainer>
   );
